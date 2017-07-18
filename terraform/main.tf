c41c6dd88d908a69b6f2ff06dd1fef05c1d874a9
tuhincicd <chinsat51@gmail.com>
2017-07-18 20:52:19 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "vault" {
     address = "http://127.0.0.1:8200"
-    token = "11ff0bb4-13f6-3d50-29eb-9754c00fbfd4"
+    token = "a8ab4d6d-f2ca-4a29-b088-7a5a078b4864"
   
 }
 
@@ -29,5 +29,17 @@ resource "aws_instance" "example" {
  }
 }
 
+resource "vault_secret" "cert" {
+    path = "/root/certs"
+}
+
+# Assuming a Vault entry with the following fields:
+#   cert
+#   key
+resource "aws_iam_server_certificate" "www" {
+    
+    certificate_body = "/root/certs/site.crt"
+    private_key = "/root/certs/ap-southeast-1-test.pem"
+}
 
 
