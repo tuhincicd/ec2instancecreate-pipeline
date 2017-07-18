2b40431ff55e625597c06e6e0181e1b0f40ce5bb
tuhincicd <chinsat51@gmail.com>
2017-07-18 22:02:57 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,4 @@
-provider "vault" {
-    address = "http://127.0.0.1:8200"
-    token = "df70b503-8951-d4e1-f580-32cc27df27df"
-  
-}
 
-resource "vault_secret" "aws" {
-    path = "/aws/creds/deploy"
-}
-
-# Assuming a Vault entry with the following fields:
-#   access_key
-#   secret_key
-
-provider "aws" {
-    access_key = "${vault_secret.aws.deploy.access_key}"
-    secret_key = "${vault_secret.aws.deploy.secret_key}"
-    region = "ap-southeast-1"
-}
 
 
 resource "aws_instance" "example" {
